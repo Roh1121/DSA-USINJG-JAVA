c9893d17f5c5c1e68032583545a70db005d4304f
Rohini Avula <22311A6925@iot.sreenidhi.edu.in>
2025-03-08 17:35:21 +0530

    Best time to Buy and Sell.java

---
+++ Best time to Buy and Sell.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int mini=prices[0];
+        int profit=0;
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i]<mini){
+                mini=prices[i];
+            }
+            profit=Math.max(profit,prices[i]-mini);
+        }
+        return profit;
+    }
+}
